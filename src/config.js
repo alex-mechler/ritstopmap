ca5d7fac8e3fe81fe2a20799dcc72233d9483f01
Alexander Shuey <alex@alexshuey.com>
2019-02-16 12:19:13 -0500

    Added favicon

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export default {
     environment: process.env.NODE_ENV,
     api_root: process.env.VUE_APP_API_ROOT || '',
     login_base: process.env.VUE_APP_LOGIN_BASE || '',
-    version: '2.3.1',
+    version: '2.3.2',
     community_day_start: process.env.VUE_APP_COMMUNITY_DAY_START || 100,
     community_day_end: process.env.VUE_APP_COMMUNITY_DAY_END || 200,
     community_day_enabled: !!process.env.VUE_APP_COMMUNITY_DAY_ENABLED
